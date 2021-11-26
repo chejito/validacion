6bf1e9831adb661106bdac52904487d007ad82b0
chejito <smndz81@gmail.com>
2021-11-26 02:17:24 +0100

    UploadImageCloudinaryServiceImpl añadido.

---
+++ src/main/java/ob/proyecto/validacion/services/UploadImageCloudinaryServiceImpl.java
@@ -0,0 +1,45 @@
+package ob.proyecto.validacion.services;
+
+import com.cloudinary.*;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Clase que implementa la subida de una imagen al
+ * servicio de alojamiento en la nube Cloudinary
+ */
+@Service
+public class UploadImageCloudinaryServiceImpl implements UploadImageService {
+
+    private final String cloudName = System.getenv("CLOUDINARY_CLOUD_NAME");
+    private final String apiKey = System.getenv("CLOUDINARY_API_KEY");
+    private final String apiSecret = System.getenv("CLOUDINARY_API_SECRET");
+    private final Map params = ObjectUtils.asMap(
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret
+    );
+
+    Cloudinary cloudinary = new Cloudinary(params);
+
+    /**
+     * Método que efectúa la subida del archivo
+     *
+     * @param photo Imagen que se recibe
+     * @return Url de la imagen alojada
+     * @throws IOException Si no se puede efectuar la subida del archivo
+     */
+    @Override
+    public String uploadImage(File photo) throws IOException {
+        System.out.println(cloudName + " " + apiKey + " " + apiSecret  );
+        Map response = cloudinary.uploader().upload((photo),
+                ObjectUtils.asMap("public_id", "olympic_flag"));
+
+        return response.get("secure_url").toString();
+    }
+}
