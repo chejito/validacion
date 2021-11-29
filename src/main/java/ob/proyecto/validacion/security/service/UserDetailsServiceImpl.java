e73d2a0e753ad1f50359b121d1f88206e5aaff97
chejito <smndz81@gmail.com>
2021-11-29 15:14:48 +0100

    Eliminadas clases no utilizadas

--- src/main/java/ob/proyecto/validacion/security/service/UserDetailsServiceImpl.java
+++ src/main/java/ob/proyecto/validacion/security/service/UserDetailsServiceImpl.java
@@ -1,19 +1,16 @@
 package ob.proyecto.validacion.security.service;
 
-import ob.proyecto.validacion.entities.Role;
 import ob.proyecto.validacion.entities.User;
-import ob.proyecto.validacion.repositories.RoleRepository;
 import ob.proyecto.validacion.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Servicio que gestiona los detalles de usuarios
