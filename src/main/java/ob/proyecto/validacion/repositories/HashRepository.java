da9b225812e356af7439d3d096f34735f0b281a5
chejito <smndz81@gmail.com>
2021-12-08 13:47:56 +0100

    HashRepository modificado

--- src/main/java/ob/proyecto/validacion/repositories/HashRepository.java
+++ src/main/java/ob/proyecto/validacion/repositories/HashRepository.java
@@ -2,12 +2,16 @@ package ob.proyecto.validacion.repositories;
 
 import ob.proyecto.validacion.entities.HashCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Respositorio de la entidad HashCode
  */
+@Repository
 public interface HashRepository extends JpaRepository<HashCode, Long> {
 
-    HashCode getByHash(Integer hash);
+    HashCode createHashCode(Integer hash);
+    HashCode getHashCode(Integer hash);
+    HashCode getHashCodeByUsername(String username);
 
 }
